1c5de7ba709fb0559787d013e7b06256b1b8d0d0
arwahjawwad <arwah.jawwad.aj@gmail.com>
2021-01-04 01:45:14 +0500

    Design changes performed

---
+++ src/components/Auth/Signin/ForgotPassword.js
@@ -0,0 +1,218 @@
+import React from 'react';
+import {Modal, TextField, Typography, Paper, Link, makeStyles, Button, IconButton} from '@material-ui/core';
+import Send from "../../../assets/Icons/send.svg";
+import Close from "../../../assets/Icons/Cross.svg";
+import SendTick from "../../../assets/Icons/resetTick.svg";
+import SendBig from "../../../assets/Icons/sendBig.svg";
+import CloseOutlinedIcon from '@material-ui/icons/CloseOutlined';
+
+const useStyles = makeStyles((theme) => ({
+    link: {
+        FontSize: "16px",
+        color: "#8A929D",
+        marginTop: ""
+    }, 
+    Paper: {
+        position: "absolute",
+        width: "43%",
+        height:"64%",
+        left: "50%",
+        top: "50%",
+        transform: "translate(-50%, -50%)",
+        outline: "none",
+        borderRadius: "20px",
+    },
+    heading: {
+        FontSize: "40px",
+        fontFamily: "EuclidCircularB",
+        fontWeight: 700,
+        marginBottom: "20px"
+    },
+    text: {
+        //marginTop: "2%",
+        width: "100%",
+        paddingBottom: "20px",
+        marginTop: "-20px"
+    },
+    Button: {
+        background: "#FAAF31",
+        borderColor: "#FAAF31",
+        margin: theme.spacing(3, 0, 2, 0),
+        color: "white",
+        '&:hover': {
+            background: "#FAAF31",
+            borderColor: "#FFFFFF"
+        }
+    },
+    body: {
+        marginTop: "2%",
+        color: "#8A929D",
+        width: "90%"
+    },
+    send: {
+        height:"44%",
+        width:"24%",
+    },
+    modal: {
+        '&:focus':{
+            borderColor: "white"
+        }
+    }
+}));
+
+export default function ForgotPassword() {
+    const classes = useStyles();
+
+    const [open, setOpen] = React.useState(false);
+    const [open1, setOpen1] = React.useState(false);
+    const [open2, setOpen2] = React.useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const handleOpen1 = () => {
+        setOpen1(true);
+    };
+
+    const handleClose1 = () => {
+        setOpen1(false);
+    };
+
+    const handleOpen2 = () => {
+        setOpen2(true);
+    };
+
+    const handleClose2 = () => {
+        setOpen2(false);
+    };
+
+    const onSendClick = () => {
+        handleClose();
+        handleOpen1();
+    }
+
+    const onNextClick = () => {
+        handleClose1();
+        handleOpen2();
+    }
+
+    const onCloseClick = () => {
+        handleClose();
+        handleClose1();
+        handleClose2();
+    }
+
+    const body = (
+        <Paper className={classes.Paper}>
+            <div align="right" style={{margin: "5px"}}>
+                 {/* <img src={Close} style={{ width:"1vw"}} onClick ={onCloseClick} /> */}
+                 <IconButton onClick ={onCloseClick}>
+                    <CloseOutlinedIcon />
+                </IconButton>
+            </div>
+                <div style={{ margin: "2%"}}>
+                <Typography variant="h4" className={classes.heading}>
+                    Forgot password? Let's help you reset it
+                </Typography>
+                <Typography variant="body2" className={classes.body}>
+                    enter your email address and we will send you the link to reset the password 
+                </Typography>
+                <br />
+                <Typography variant = "body2">
+                    Email Address
+                </Typography>
+                <TextField variant = "outlined" 
+                placeholder = "Type your email address here"
+                className={classes.text}
+                style = {{ marginTop: "0px"}}
+                /> 
+                <Button variant="outlined" className={classes.Button} onClick ={onSendClick}>
+                    Send 
+                    <img src={Send} style={{marginLeft:"5px", width:"1.1vw"}}/>
+                </Button>
+            </div>
+        </Paper>
+    );
+    
+    const body1 = (
+        //height = 42%  width = 43%
+        <Paper className={classes.Paper} style ={{height: "42%"}}>
+            <div align="right" style={{margin: "5px"}}>
+                {/* <img src={Close} style={{ width:"1vw"}} onClick ={onCloseClick} /> */}
+                <IconButton onClick ={onCloseClick}>
+                    <CloseOutlinedIcon />
+                </IconButton>
+            </div>
+            <div style={{ margin: "5%"}}>
+                <Typography variant = "h4" className = {classes.heading}>
+                    Reset link sent to your email
+                    <img src = {SendTick} style ={{marginLeft: "20px", width: "3vw"}}/>
+                </Typography>
+                <Typography variant = "body2" className = {classes.body} >
+                    Please check your inbox and click on the link to rest your password
+                </Typography>
+                <Button variant="outlined" className={classes.Button} onClick ={onNextClick}>
+                    Next
+                </Button>
+                <img src = {SendBig} className ={classes.send} align = "right" />
+            </div>
+            
+        </Paper>
+    );
+
+    const body2 = (
+        <Paper className ={classes.Paper}>
+            <div align="right" style={{margin: "5px"}} >
+                 {/* <img src={Close} style={{ width:"1vw"}} onClick ={onCloseClick} /> */}
+                 <IconButton onClick ={onCloseClick}>
+                    <CloseOutlinedIcon />
+                </IconButton>
+            </div>
+            <div style={{ margin: "5%"}}>
+                <Typography variant = "h4" className = {classes.heading}>
+                    Create a new password    
+                </Typography>
+                <Typography variant = "body2">Create a password</Typography> <br />
+                    <TextField variant = "outlined" 
+                    placeholder = "Type your password here"
+                    className={classes.text}
+                    /> 
+                    <br />
+                    <Typography variant = "body2">Confirm password</Typography> <br />
+                    <TextField variant = "outlined" 
+                    placeholder = "Type your password here"
+                    className={classes.text}
+                    /> 
+                    <br />
+                    <Button variant="outlined" className={classes.Button} >
+                        Reset Password
+                    </Button>
+            </div>
+        </Paper>
+    );
+    return (
+      <div>
+        <Typography variant="body2">
+            <Link align ="right" onClick={handleOpen} className={classes.link} href="#">
+                Forgot your password?
+            </Link>
+        </Typography>
+
+        <Modal open={open} onClose={handleClose} className = {classes.modal}>
+            {body}
+        </Modal>
+
+        <Modal open={open1} onClose={handleClose1} className = {classes.modal}>
+            {body1}
+        </Modal>
+
+        <Modal open={open2} onClose={handleClose2} className = {classes.modal}>
+            {body2}
+        </Modal>
+      </div>
+    )
+}
---
+++ src/components/Navigation/Header/Header.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import {Button, makeStyles, Box, AppBar,Toolbar,} from '@material-ui/core';
+import Logo from "../../../assets/Icons/Icon.svg"
+
+const useStyles = makeStyles((theme) => ({
+    signup: {
+        background: "#FFF0D5",
+        borderColor: "#FFF0D5",
+        color: "#FCA200"
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        background: "white",
+        height: "15vh"
+      },
+    grow: {
+        flexGrow: 1,
+    },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  dummySidebar: {
+    [theme.breakpoints.up("sm")]: { marginTop: "0px" },
+    [theme.breakpoints.down("sm")]: { marginTop: "37px" },
+  },
+}))
+
+export default function Header(props){
+
+    const classes = useStyles();
+
+    return(
+
+        
+    <div className={classes.grow}>
+    <AppBar position="fixed" className={classes.appBar}>
+      <Toolbar>
+        <img src={Logo}className={classes.logo} />
+        <div className={classes.grow} />
+        <div className={classes.sectionDesktop}>
+        <Box
+          style={{ width: "100%" }}
+          display="flex"
+          flexDirection="row"
+          p={1}
+        >
+          <Box>
+            <Button 
+            style={{color:"#707070"}}
+            href="/"
+            >
+             Login
+            </Button>          
+          </Box>
+          <Box>
+            <Button variant="outlined"
+            className={classes.signup}
+            href="/signup"
+            >
+                Signup
+            </Button>
+          </Box>
+        </Box>
+        </div>
+        <div className={classes.sectionMobile}>
+        <Box
+          style={{ width: "100%" }}
+          display="flex"
+          flexDirection="row"
+          p={1}
+        >
+          <Box>
+            <Button 
+            style={{color:"#707070"}}
+            >
+             Login
+            </Button>          
+          </Box>
+          <Box>
+            <Button variant="outlined"
+            className={classes.signup}
+            >
+                Signup
+            </Button>
+          </Box>
+        </Box>
+        </div>
+      </Toolbar>
+    </AppBar>
+    <div className={classes.dummySidebar}>
+        {props.children}
+    </div>
+    </div>
+    )
+
+};
